a4da8524d88287d222f16ee2d3cb9c18bec712d7
kmart-tech <70730317+khmartinsen@users.noreply.github.com>
2022-02-07 20:17:01 -0700

    Expand Coset class

--- src/main/java/bspq/bspqtools/Coset.java
+++ src/main/java/bspq/bspqtools/Coset.java
@@ -1,16 +1,62 @@
 package bspq.bspqtools;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Coset {
     final int firstZero;
     final int lastZero;
-    int[] coordinates;
+    public final int[] coordinates;
     String path;
+    ArrayList<String> moves;
+    int lastMoveOffset = 0;
 
-    Coset(int[] array) {
+
+    public Coset(int[] array, String path) {
         firstZero = array[0];
         lastZero = array[1];
         coordinates = Arrays.copyOfRange(array, 2, array.length);
+        this.path = path;
+        moves = parsePathString(path);
+
+        String lastMove = moves.get(moves.size() - 1);
+
+        for (int i = 0; i < lastMove.length(); i++) {
+            if (lastMove.charAt(i) == 'B') lastMoveOffset--;
+            else if (lastMove.charAt(i) == 'b') lastMoveOffset++;
+        }
+
+    }
+
+    public int getFirstZero() {
+        return firstZero;
+    }
+
+    public int getLastZero() {
+        return lastZero;
+    }
+
+    public int[] getCoordinates() {
+        return coordinates;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public static ArrayList<String> parsePathString(String path) {
+        ArrayList<String> moves = new ArrayList<>();
+        int start = 0;
+        for(int i = 0; i < path.length(); i++) {
+            if (path.charAt(i) == 't' || path.charAt(i) == 'T') {
+                moves.add(path.substring(start, i));
+                start = i + 1;
+            }
+        }
+        return moves;
     }
 }
+
+
+
