b4a27dba49dcce7440150be1c08f80fa0e15be9b
khmartinsen <khmartinsen@gmail.com>
2022-04-08 10:04:27 -0600

    work with the bash script more

--- src/main/bspqscript.sh
+++ src/main/bspqscript.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+# usage: bspqscript p q
 echo "BSPQ master script"
-echo "Enter"
+P=$1
+Q=$2
+DIR=$(pwd)
+
+#runs coset builder in another shell since we change the directory
+builder() {
+  ${DIR}/bin/cosetBuilder $P $Q $1 $2
+}
+
+accumulate() {
+  ${DIR}/bin/accumulate #1
+}
+
+cd data/BS${P}_${Q}
+
+builder mainline BTBT
 
